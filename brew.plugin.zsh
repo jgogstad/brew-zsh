a936553c8d78023f8c6b9da1cd89be5796af5c91
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-03-24 10:55:22 +0100

    Added comment about missing shas

--- brew.plugin.zsh
+++ brew.plugin.zsh
@@ -3,4 +3,10 @@ export PATH="/usr/local/sbin:$PATH"
 
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_INSECURE_REDIRECT=1
+
+# Certain casks comes without checksums, notably Spotify; these packages should not be installed with homebrew.
+# Look for the packages with `mas` (or just use the App Store), or download directly from the vendor. They (should) come
+# with built-in update mechanisms that patches the current installation rather than replacing it like homebrew does.
+# This will save you from the "This application has been downloaded from the internet…" dialog every time the
+# application updates as well.
 export HOMEBREW_CASK_OPTS=--require-sha
